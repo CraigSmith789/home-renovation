8564b8d927e8824009d6b90ae59ed0fe35075476
CraigSmith789 <craig789@gmail.com>
2021-01-17 14:50:31 -0500

    Contractor controller

--- app/controllers/contractors_controller.rb
+++ app/controllers/contractors_controller.rb
@@ -1,2 +1,43 @@
 class ContractorsController < ApplicationController
+
+  def index
+    @contractor = Contractor.all
+  end
+
+  def create
+    @contractor = Contractor.new(contractor_params)
+    @contractor.save
+    redirect_to contractor_path(@contractor)
+
+    def new
+      @contractor = Contractor.new
+    end
+
+    def edit 
+      @contractor = Contractor.find(params[:id])
+    end
+
+    def show
+      @contractor = Contractor.find(params[:id])
+    end
+
+    def update
+      @contractor = C0ntractor.find_by(id: params[:id])
+      @contractor.update(attraction_params)
+      redirect_to contractor_path(contractor)
+    end
+
+    def destroy
+      @contractor = Contractor.find(params[:id])
+      @contractor.destroy
+      redirect_to contractors_path
+    end
+
+
+    private
+
+  def contractor_params
+    params.require(:contractor).permit(:name, :email)
+  end
+
 end
