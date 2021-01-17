cae7696a01b27ef65cda948fb1f32ecd3c089b71
CraigSmith789 <craig789@gmail.com>
2021-01-17 16:50:03 -0500

    User model and controller Sessions controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_17_185432) do
+ActiveRecord::Schema.define(version: 2021_01_17_214329) do
 
   create_table "contractors", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2021_01_17_185432) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
