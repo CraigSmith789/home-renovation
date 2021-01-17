a8eb3a5e9427b333e8e7b0b73a2eac83ba09c87d
CraigSmith789 <craig789@gmail.com>
2021-01-17 13:56:52 -0500

    Models and Controllers

---
+++ db/migrate/20210117183259_create_homes.rb
@@ -0,0 +1,13 @@
+class CreateHomes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :homes do |t|
+      t.string :name
+      t.integer :year_built
+      t.integer :sf
+      t.integer :beds
+      t.integer :baths
+
+      t.timestamps
+    end
+  end
+end
