faf2c919a773e9d29688f450aae4a7facc16d51a
CraigSmith789 <craig789@gmail.com>
2021-01-22 21:50:14 -0500

    remove byebug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    byebug
     if (user = User.create(user_params))
       session[:user_id] = user.id
       redirect_to homes_path
