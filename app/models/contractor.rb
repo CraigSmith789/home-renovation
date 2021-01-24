ac0b5dbaa7a2eeae02e31a8c867bfd3de4f40cff
CraigSmith789 <craig789@gmail.com>
2021-01-23 21:06:02 -0500

    404 and views

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -1,11 +1,6 @@
 class Contractor < ApplicationRecord
   has_many :projects
-  has_many :tasks,dependent: :destroy
+  has_many :tasks, dependent: :destroy
   has_many :homes, through: :projects
 
-  # validates :name, presence: true
-  # validates :email,
-  # format: { with: /^(.+)@(.+)$/, message: "Email invalid"  },
-  #           uniqueness: { case_sensitive: false },
-  #           length: { minimum: 4, maximum: 254 }   
 end
--- app/models/home.rb
+++ app/models/home.rb
@@ -10,6 +10,7 @@ class Home < ApplicationRecord
   validates :beds, presence: true, numericality: true
   validates :baths, presence: true, numericality: true
   scope :lead_paint_risk, -> { where("year_built < 1978") }
+  scope :order_by_date, -> {year_built :desc}
 
   def self.order_by_date
     self.order(year_built: :desc) 
